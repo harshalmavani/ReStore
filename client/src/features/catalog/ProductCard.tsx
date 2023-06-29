ebdd022ec98e5dfbb82dba930ab744be0edc64d8
Harshal Mavani <harshalmavani01@gmail.com>
2023-06-30 00:25:58 +0530

    initial commit of frontend

---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,31 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { Product } from "../../app/models/product";
+
+interface Prop {
+  product: Product;
+}
+
+export default function ProductCard({ product }: Prop) {
+  return (
+    <Card>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={product.pictureUrl}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
