d3e8826cf80bf919ae1f60b5535617ea9b3bfa73
Harshal Mavani <harshalmavani01@gmail.com>
2023-07-28 10:20:43 +0530

    section 7 : 008

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,7 +1,31 @@
-import { Typography } from '@mui/material'
+import { Button, ButtonGroup, Divider, Typography } from '@mui/material'
+import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
+import { decrement, increment } from './counterSlice';
 
 export default function ContactPage() {
-  return (
-    <Typography variant='h2'>ContactPage</Typography>
-  )
+	// const { data, title } = useSelector((state: CounterState) => state);
+	const { data, title } = useAppSelector(state => state.counter);
+	const dispatch = useAppDispatch();
+	return (
+		<>
+			<Typography variant='h2'>
+				{title}
+			</Typography>
+			<Typography variant='h5'>
+				The data is: {data}
+			</Typography>
+			<Divider sx={{ mb: 2 }} />
+			<ButtonGroup>
+				<Button onClick={() => dispatch(decrement(1))} variant='contained' color='error'>
+					Decrement 1
+				</Button>
+				<Button onClick={() => dispatch(increment(1))} variant='contained' color='primary'>
+					increment 1
+				</Button>
+				<Button onClick={() => dispatch(increment(5))} variant='contained' color='secondary'>
+					increment 5
+				</Button>
+			</ButtonGroup>
+		</>
+	)
 }
