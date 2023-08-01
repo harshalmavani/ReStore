381d68b3c0a75d26c16b2998b66831c0a62d1464
Harshal Mavani <harshalmavani01@gmail.com>
2023-08-01 10:20:24 +0530

    section 08 : 004

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,30 +1,39 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class ProductsController : BaseController
-    {
-        private readonly StoreContext _storeContext;
+	public class ProductsController : BaseController
+	{
+		private readonly StoreContext _storeContext;
 
-        public ProductsController(StoreContext storeContext)
-        {
-            _storeContext = storeContext;
-        }
+		public ProductsController(StoreContext storeContext)
+		{
+			_storeContext = storeContext;
+		}
 
-        [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(){
-            return await _storeContext.Products.ToListAsync();
-        }
+		[HttpGet]
+		public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm, string brands, string types)
+		{
+			var query = _storeContext.Products
+				.Sort(orderBy)
+				.Search(searchTerm)
+				.Filter(brands, types)
+				.AsQueryable();
 
-        [HttpGet]
-        [Route("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id){
-            var product = await _storeContext.Products.FindAsync(id);
-			if(product is null) return BadRequest(new ProblemDetails{Title = "Product not found"});
+			return await query.ToListAsync();
+		}
+
+		[HttpGet]
+		[Route("{id}")]
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			var product = await _storeContext.Products.FindAsync(id);
+			if (product is null) return BadRequest(new ProblemDetails { Title = "Product not found" });
 			return product;
-        }
-    }
+		}
+	}
 }
