c7202c4ac6cb31ab0296bb61c356b43c5b15cb8c
Harshal Mavani <harshalmavani01@gmail.com>
2023-08-02 10:18:34 +0530

    section 08 : 008

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,24 +18,37 @@ namespace API.Controllers
 		}
 
 		[HttpGet]
-		public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm, string brands, string types)
+		public async Task<ActionResult<List<Product>>> GetProducts([FromQuery] ProductParams productParams)
 		{
 			var query = _storeContext.Products
-				.Sort(orderBy)
-				.Search(searchTerm)
-				.Filter(brands, types)
+				.Sort(productParams.OrderBy)
+				.Search(productParams.SearchTerm)
+				.Filter(productParams.Brands, productParams.Types)
 				.AsQueryable();
 
-			return await query.ToListAsync();
+			var products = await PagedList<Product>.toPagedList(query,
+				productParams.PageNumber, productParams.PageSize);
+
+			Response.AddPaginationHeader(products.MetaData);
+
+			return products;
 		}
 
-		[HttpGet]
-		[Route("{id}")]
+		[HttpGet("{id}")]
 		public async Task<ActionResult<Product>> GetProduct(int id)
 		{
 			var product = await _storeContext.Products.FindAsync(id);
 			if (product is null) return BadRequest(new ProblemDetails { Title = "Product not found" });
 			return product;
+		}
+
+		[HttpGet("filters")]
+		public async Task<IActionResult> GetFilters()
+		{
+			var brands = await _storeContext.Products.Select(p => p.Brand).Distinct().ToListAsync();
+			var types = await _storeContext.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+			return Ok(new { brands, types });
 		}
 	}
 }
