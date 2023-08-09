c56d7e8947b8dcd8d1f20efd9498667dd39277f0
Harshal Mavani <harshalmavani01@gmail.com>
2023-08-09 10:27:57 +0530

    end of section 8

--- client/src/app/util/util.ts
+++ client/src/app/util/util.ts
@@ -6,3 +6,7 @@ export function getCookie(key: string) {
 export function currencyFormat(amount: number) {
 	return '$' + (amount / 100).toFixed(2);
 }
+
+export function removeEmpty(obj: object) {
+	return Object.fromEntries(Object.entries(obj).filter((v) => v != null));
+}
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -2,11 +2,12 @@ import { useEffect } from "react";
 import ProductList from "./ProductList";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, fetchFiltersAsync, productSelectors, setProductParams } from "./catalogSlice";
-import { Box, Grid, Pagination, Paper, Typography } from "@mui/material";
+import { fetchProductsAsync, fetchFiltersAsync, productSelectors, setProductParams, setPageNumber } from "./catalogSlice";
+import { Grid, Paper } from "@mui/material";
 import ProductSearch from "./ProductSearch";
 import RadioButtonGroup from "../../app/components/RadioButtonGroup";
 import CheckBoxButtons from "../../app/components/CheckBoxButtons";
+import AppPagination from "../../app/components/AppPagination";
 
 const sortOptions = [
 	{ value: 'name', label: 'Alphabetical' },
@@ -16,7 +17,7 @@ const sortOptions = [
 
 export default function Catalog() {
 	const products = useAppSelector(productSelectors.selectAll);
-	const { productsLoaded, filtersLoaded, status, brands, types, productParams } = useAppSelector(state => state.catalog)
+	const { productsLoaded, filtersLoaded, brands, types, productParams, metaData } = useAppSelector(state => state.catalog)
 	const dispatch = useAppDispatch();
 
 	useEffect(() => {
@@ -27,11 +28,11 @@ export default function Catalog() {
 		if (!filtersLoaded) dispatch(fetchFiltersAsync());
 	}, [dispatch, filtersLoaded]);
 
-	if (status.includes('pending')) return <LoadingComponent message='Loading products...' />
+	if (!filtersLoaded) return <LoadingComponent message='Loading products...' />
 
 	return (
 		<>
-			<Grid container spacing={4}>
+			<Grid container columnSpacing={4}>
 				<Grid item xs={3}>
 					<Paper sx={{ mb: 2 }}>
 						<ProductSearch />
@@ -62,13 +63,12 @@ export default function Catalog() {
 					<ProductList products={products} />
 				</Grid>
 				<Grid item xs={3} />
-				<Grid item xs={9}>
-					<Box display='flex' justifyContent='space-between' alignItems='center'>
-						<Typography>
-							Displaying 1-6 of 20 items
-						</Typography>
-						<Pagination color="secondary" size="large" count={10} page={2} />
-					</Box>
+				<Grid item xs={9} sx={{ mb: 2 }}>
+					{metaData &&
+						<AppPagination
+							metaData={metaData}
+							onPageChange={(page: number) => dispatch(setPageNumber({ pageNumber: page }))}
+						/>}
 				</Grid>
 			</Grid>
 		</>
