225347c8631fd642a32584b710737341c2294a52
Harshal Mavani <harshalmavani01@gmail.com>
2023-08-22 10:04:12 +0530

    section 09 : 021

--- client/src/features/account/Register.tsx
+++ client/src/features/account/Register.tsx
@@ -5,22 +5,41 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { Alert, AlertTitle, List, ListItem, ListItemText, Paper } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Paper } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 import { FieldValues, useForm } from 'react-hook-form';
 import { LoadingButton } from '@mui/lab';
 import agent from '../../app/api/agent';
-import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 export default function Register() {
-	const [validationErrors, setValidationErrors] = useState([]);
-	const { register, handleSubmit, formState: { isSubmitting, errors, isValid } } = useForm({
+	const navigate = useNavigate();
+
+	const { register, handleSubmit, setError, formState: { isSubmitting, errors, isValid } } = useForm({
 		mode: 'onTouched'
 	});
 
+	function handleApiErrors(errors: any) {
+		if (errors) {
+			errors.forEach((error: string) => {
+				if (error.includes('Username')) {
+					setError('username', { message: error })
+				} else if (error.includes('Email')) {
+					setError('email', { message: error })
+				} else if (error.includes('Password')) {
+					setError('password', { message: error })
+				}
+			});
+		}
+	}
+
 	function formSubmit(data: FieldValues) {
 		agent.Account.register(data)
-			.catch(error => setValidationErrors(error));
+			.then(() => {
+				toast.success('Registration successful - you can now login');
+				navigate('/login');
+			})
+			.catch(error => handleApiErrors(error));
 	}
 
 	return (
@@ -46,7 +65,9 @@ export default function Register() {
 					fullWidth
 					label="Username"
 					autoFocus
-					{...register('username', { required: 'Username is required' })}
+					{...register('username', {
+						required: 'Username is required'
+					})}
 					error={!!errors.username}
 					helperText={errors?.username?.message}
 				/>
@@ -54,7 +75,13 @@ export default function Register() {
 					margin="normal"
 					fullWidth
 					label="Email"
-					{...register('email', { required: 'Email is required' })}
+					{...register('email', {
+						required: 'Email is required',
+						pattern: {
+							value: /^\w+[\w-.]*@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/,
+							message: 'Not a valid email address'
+						}
+					})}
 					error={!!errors.email}
 					helperText={errors?.email?.message}
 				/>
@@ -63,24 +90,16 @@ export default function Register() {
 					fullWidth
 					label="Password"
 					type="password"
-					{...register('password', { required: 'Password is required' })}
+					{...register('password', {
+						required: 'Password is required',
+						pattern: {
+							value: /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/,
+							message: 'Password does not meet complexity requirements'
+						}
+					})}
 					error={!!errors.password}
 					helperText={errors?.password?.message}
 				/>
-				{validationErrors.length > 0 &&
-					<Alert severity="error">
-						<AlertTitle>Validation Errors</AlertTitle>
-						<List>
-							{validationErrors.map(error => (
-								<ListItem key={error}>
-									<ListItemText>
-										{error}
-									</ListItemText>
-								</ListItem>
-							))}
-						</List>
-					</Alert>
-				}
 				<LoadingButton
 					disabled={!isValid}
 					loading={isSubmitting}
